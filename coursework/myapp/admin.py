90bec0fa5a1776b2ed1f2ac1fac9090df0a29a49
DaemonLife <ex@mple>
2020-12-07 18:55:32 +0300

    start creation models

---
+++ coursework/myapp/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+# * Import:
+# from .model import
+
+# * Registration:
+# admin.site.register()
---
+++ coursework/myapp/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# * My models:
+
+class Group(models.Model):
+    # ? automated id
+    name = models.CharField(max_length=31)
+    numbers = models.PositiveIntegerField()
+
+class discipline(models.Model):
+    name = models.CharField(max_length=127)
+
+class teacher(model.Model):
+    surname = models.CharField(max_length=31)
+    name = models.CharField(max_length=31)
+    patronymic = models.CharField(max_length=31)
+    position = models.CharField(max_length=31)
