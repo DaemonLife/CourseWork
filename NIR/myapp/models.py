50aefdcb4e34c85c67458805b8dfbe95928be969
DaemonLife <ex@mple>
2020-12-20 18:30:37 +0300

    ...

--- NIR/myapp/models.py
+++ NIR/myapp/models.py
@@ -2,20 +2,67 @@ from django.db import models
 
 # * My models:
 
-class Group(models.Model):
-    # ? automated id
-    name = models.CharField(max_length=31)
-    numbers = models.PositiveIntegerField()
-
-class discipline(models.Model):
-    name = models.CharField(max_length=127)
-
-class teacher(models.Model):
+# Преподаватель
+class Teacher(models.Model):
     surname = models.CharField(max_length=31)
     name = models.CharField(max_length=31)
     patronymic = models.CharField(max_length=31)
     position = models.CharField(max_length=31)
+    chair = models.ForeignKey('Chair', on_delete=models.SET_NULL)
+    work_time = models.OneToOneField('Work_time', on_delete = models.SET_NULL)
 
+# Кафедра
+class Chair(models.Model):
+    name = models.CharField(max_length=31)
+    faculty = models.ForeignKey('Faculty', on_delete = models.SET_NULL)
+
+# Рабочее время преподавателя
+class Work_time(models.Model):
+    # monday = 
+    # tuesday = 
+    # wednesday =
+    # thursday = 
+    # friday = 
+    corps_index = models.CharField(max_length=7)
+    corps_number = models.PositiveIntegerField()
+
+# Факультет
+class Faculty(models.Model):
+    name = models.CharField(max_length=31)
+
+# Направление
+class  Direction (models.Model):
+    code = models.CharField(max_length=15, primary_key=True) # id
+    name = models.CharField(max_length=31)
+    chair = models.ForeignKey('Chair', on_delete=models.SET_NULL)
+    qualification = models.CharField(max_length=63)
+
+# Группа
+class Group(models.Model): 
+    name = models.CharField(max_length=31)
+    amount = models.PositiveIntegerField()
+    direction = models.ForeignKey('Direction', on_delete=models.SET_NULL)
+
+# Подгруппа
+class Subgroup(models.Model):
+    amount = models.PositiveIntegerField()
+    group_parent = models.OneToOneField('Group', on_delete=models.SET_NULL)
+
+# Дисциплина
+class Discipline(models.Model):
+    name = models.CharField(max_length=127)
+
+# Расписание занятия
+class Class_schedule(models.Model):
+    pass
+
+# Экзамен
+class Exam(models.Model):
+    day = models.DateField()
+    groups = models.ForeignKey(Group, on_delete=models.SET_NULL)
+
+class Discipline_schedule(models.Model):
+    pass
 
 # ! Литобзор
 
